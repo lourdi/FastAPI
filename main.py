b6f9fbee5da9f02c9a5e67a0f4d0fa1cccf61aea
lourdi <38117292+lourdi@users.noreply.github.com>
2023-09-11 07:34:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from typing import List
+import camelot
+import os
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+@app.post("/pdf-to-csv/")
+async def pdf_to_csv(files: List[UploadFile]):
+    csv_files = []
+
+    for uploaded_file in files:
+        try:
+            # Save the uploaded PDF file temporarily
+            with open("temp.pdf", "wb") as pdf_file:
+                pdf_file.write(uploaded_file.file.read())
+
+            # Use Camelot-Py to extract tables from the PDF
+            tables = camelot.read_pdf("temp.pdf")
+
+            # Convert each table to CSV
+            for i, table in enumerate(tables):
+                csv_file_path = f"table_{i + 1}.csv"
+                table.to_csv(csv_file_path)
+                csv_files.append(csv_file_path)
+
+            return JSONResponse(content={"success": "PDF to CSV conversion successful", "csv_files": csv_files})
+
+        except Exception as e:
+            return JSONResponse(content={"error": str(e)})
+        finally:
+            # Remove the temporary PDF file
+            os.remove("temp.pdf")
+
